c24814df636577ea6fcebefd38de0fe21acf16fb
Paul.Jacks <pjacks9168@stu.d214.org>
2018-10-24 13:50:20 -0500

    Initial Commit

---
+++ CollegeProfileBuilder - Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollegeProfileBuilder - Firebase
+//
+//  Created by Paul Jacks on 10/24/18.
+//  Copyright © 2018 Paul Jacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
